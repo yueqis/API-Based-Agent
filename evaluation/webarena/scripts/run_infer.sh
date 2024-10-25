8f19181fe266446e383a4a5b921bd7a247bb28f0
Yueqi Song <141804823+yueqis@users.noreply.github.com>
2024-10-25 15:15:42 -0400

    Update run_infer.sh

--- evaluation/webarena/scripts/run_infer.sh
+++ evaluation/webarena/scripts/run_infer.sh
@@ -4,13 +4,14 @@ AGENT="CodeActAgent"
 # IMPORTANT: Because Agent's prompt changes fairly often in the rapidly evolving codebase of OpenDevin
 # We need to track the version of Agent in the evaluation to make sure results are comparable
 AGENT_VERSION=v$(poetry run python -c "import agenthub; from opendevin.controller.agent import Agent; print(Agent.get_cls('$AGENT').VERSION)")
-export log_file='map_wiki.log'
-COMMAND="poetry run python evaluation/gitlab-mix/run_infer.py \
+export log_file='log.log'
+
+COMMAND="poetry run python evaluation/webarena/run_infer.py \
   --agent-cls CodeActAgent \
   --llm-config llm \
   -m openai/gpt-4o
   -e SSH_PASSWORD='"hello"'
-  --start_task_id 400 \
+  --start_task_id 0 \
   --eval-n-limit 1 \
   --max-iterations 18 \
   --data-split validation \
